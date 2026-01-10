a5d10901b56bbafb7ac02883e519c87f17d259e3
Ethan Henry <ehtanhnery@gmail.com>
2026-01-09 21:17:15 -0800

    Add TeamCodeC

---
+++ TeamCodeA/src/main/java/org/firstinspires/ftc/teamcodea/opmode/MoveMotorForward.java
@@ -0,0 +1,21 @@
+package org.firstinspires.ftc.teamcodea.opmode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+
+@TeleOp
+public class MoveMotorForward extends OpMode {
+    private DcMotor motor;
+    @Override
+    public void init() {
+        // Initialize motor here
+        motor = hardwareMap.get(DcMotorEx.class, "flipper");
+    }
+
+    @Override
+    public void loop() {
+        motor.setPower(1);
+    }
+}
